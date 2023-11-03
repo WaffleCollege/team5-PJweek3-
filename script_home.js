dd8fed053bbbbdd48ae354b6d0d0f243d0a66f52
tutumi <21241215@kyoto-wu.ac.jp>
2023-11-03 21:21:06 +0900

    20231103

---
+++ script_home.js
@@ -0,0 +1,22 @@
+'use strict'
+// Please don't delete the 'use strict' line above
+
+function sampleWin_aya() {
+    window.open("aya.html", "swindow", "toolbar=0, menubar=0, scrollbars=auto, width=1100, height=1000")
+}
+
+function sampleWin_kinari() {
+    window.open("kinari.html", "swindow", "toolbar=0, menubar=0, scrollbars=auto, width=1100, height=1000")
+}
+
+function sampleWin_tutumi() {
+    window.open("tutumi.html", "swindow", "toolbar=0, menubar=0, scrollbars=auto, width=1100, height=1000")
+}
+
+function sampleWin_mana() {
+    window.open("mana.html", "swindow", "toolbar=0, menubar=0, scrollbars=auto, width=1100, height=1000")
+}
+
+function sampleWin_yosshi() {
+    window.open("yosshi.html", "swindow", "toolbar=0, menubar=0, scrollbars=auto, width=1100, height=1000")
+}
---
+++ script_interest.js
@@ -0,0 +1,63 @@
+const appear1 =　document.querySelector("#name1")
+const appeari1 = document.querySelector("#img1")
+const appeart1 = document.querySelector("#te1")
+const button1 = document.querySelector("#button1")
+
+button1.addEventListener('click', () => {
+  appear1.classList.toggle('name1');
+  appeari1.classList.toggle('img1');
+  appeart1.classList.toggle('te1');
+});
+
+
+const appear2 =　document.querySelector("#name2")
+const appeari2 = document.querySelector("#img2")
+const appeart2 = document.querySelector("#te2")
+const button2 = document.querySelector("#button2")
+
+button2.addEventListener('click', () => {
+  appear2.classList.toggle('name2');
+  appeari2.classList.toggle('img2');
+  appeart2.classList.toggle('te2');
+});
+
+const appear3 =　document.querySelector("#name3")
+const appeari3 = document.querySelector("#img3")
+const appeart3 = document.querySelector("#te3")
+const button3 = document.querySelector("#button3")
+
+button3.addEventListener('click', () => {
+  appear3.classList.toggle('name3');
+  appeari3.classList.toggle('img3');
+  appeart3.classList.toggle('te3');
+});
+
+const appear4 =　document.querySelector("#name4")
+const appeari4 = document.querySelector("#img4")
+const appeart4 = document.querySelector("#te4")
+const button4 = document.querySelector("#button4")
+
+button4.addEventListener('click', () => {
+  appear4.classList.toggle('name4');
+  appeari4.classList.toggle('img4');
+  appeart4.classList.toggle('te4');
+});
+
+const appear5 =　document.querySelector("#name5")
+const appeari5 = document.querySelector("#img5")
+const appeart5 = document.querySelector("#te5")
+const button5 = document.querySelector("#button5")
+
+button5.addEventListener('click', () => {
+  appear5.classList.toggle('name5');
+  appeari5.classList.toggle('img5');
+  appeart5.classList.toggle('te5');
+});
+
+
+const btn =document.querySelector("#btn")
+const into = document.querySelector("#into")
+btn.addEventListener('click',()  => {
+  into.style.fontSize = "300px"
+});
+
